6a35c255c3b3f8bb0eb23a7ec1de8e9e0e0438c4
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 11:04:50 +0700

    Session example login

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.use(session({secret: "Shh, its a secret!"}));
 app.get('/', function(req, res){
    if(req.session.page_views){
       req.session.page_views++;
-      res.send("You visited this page " + req.session.page_views + " times");
+      res.send("You visited this page " + req.session.page_views + " times with User: "+req.session.login);
    } else {
+      req.session.login = "USER1";
+      req.session.password = "PASSWORDNYA";
       req.session.page_views = 1;
-      res.send("Welcome to this page for the first time!");
+      res.send("Welcome to this page for the first time!<button>A</button>");
    }
 });
 app.listen(3000);
