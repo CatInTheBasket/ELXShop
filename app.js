f89ee38f51edba43655fcfdc5a3f0233223d53f7
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 14:39:06 +0700

    todo

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ app.get('/', function(req, res){
    
 });
 app.get('/register',function(req,res){
+   res.render('formRegister');
+})
+app.post('/register',function(req,res){
 
    res.render('formRegister');
 })
-
 app.get('/homeAdmin',function(req,res){
 
    if(req.session.role=="Admin"){
