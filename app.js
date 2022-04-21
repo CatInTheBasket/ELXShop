68897b453fd0e878572f8586f6c1b1fb6dae60da
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 11:16:22 +0700

    Session example

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ var app = express();
 
 app.use(cookieParser());
 app.use(session({secret: "Shh, its a secret!"}));
-
+app.set('view engine','ejs' )
 app.get('/', function(req, res){
+   if(!req.query.username || !req.query.password ){
+      res.render('formLogin');
+   }else{
+      req.session.login = req.query.username;
+      req.session.password = req.query.password;
+      res.redirect('/home');
+   }
+   
+});
+app.get('/home',function(req,res){
+
    if(req.session.page_views){
       req.session.page_views++;
       res.send("You visited this page " + req.session.page_views + " times with User: "+req.session.login);
    } else {
-      req.session.login = "USER1";
-      req.session.password = "PASSWORDNYA";
       req.session.page_views = 1;
-      res.send("Welcome to this page for the first time!<button>A</button>");
+      res.send("Welcome to this page for the first time!");
    }
-});
+})
 app.listen(3000);
