90eb5bd97a2ec4d8ae7dd4289442598f07dbaa8c
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 22:28:56 +0700

    upload gambar

--- controller/controller.js
+++ controller/controller.js
@@ -96,7 +96,7 @@ class Controller {
                     }).catch(err => {
                         console.log(err);
                         res.send(err);
-                    });
+                    }); 
                 }
             })
         }
