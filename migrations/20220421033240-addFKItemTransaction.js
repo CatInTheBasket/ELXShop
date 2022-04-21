b94c8bf5de759bb14f9c2faa1385dd73c4eda4fe
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 10:49:54 +0700

    init done

--- migrations/20220421033240-addFKItemTransaction.js
+++ migrations/20220421033240-addFKItemTransaction.js
@@ -5,11 +5,17 @@ module.exports = {
     return queryInterface.addColumn('ItemTransactions', 'ProductId', {
       type: Sequelize.INTEGER,
       references: { model: 'Products', key: 'id' }
-    })
+    }).then(result=>{
+      queryInterface.addColumn('ItemTransactions', 'TransactionId', {
+        type: Sequelize.INTEGER,
+        references: { model: 'Transactions', key: 'id' }
+    })})
   },
 
   down(queryInterface, Sequelize) {
 
-    return queryInterface.removeColumn('ItemTransactions', 'ProductId', {})
+    return queryInterface.removeColumn('ItemTransactions', 'ProductId', {}).then(result=>{
+      queryInterface.removeColumn('ItemTransactions', 'TransactionId', {})
+    })
   }
 };
