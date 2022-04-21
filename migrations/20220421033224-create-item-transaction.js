88261b1363a859a8094f479d05d86540580f663b
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-21 10:52:29 +0700

    Fix async

--- migrations/20220421033224-create-item-transaction.js
+++ migrations/20220421033224-create-item-transaction.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ItemTransactions', {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('ItemTransactions', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -21,7 +21,7 @@ module.exports = {
       }
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ItemTransactions');
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('ItemTransactions');
   }
 };
