a4f7a3f02e0b4b58e2ed37b8bcd7a230af608ee2
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-04-22 08:19:47 +0700

    test

--- app.js
+++ app.js
@@ -22,4 +22,6 @@ app.use('/', router);
 
 app.set('view engine','ejs' )
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
